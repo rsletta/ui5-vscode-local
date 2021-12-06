e8930427a6adade6a644c12db79920f6c636fdc6
Luiz Gomes <luiz.gomes@spro.com.br>
2021-12-06 09:11:02 -0300

    conected to some-api

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,5 +1,20 @@
 sap.ui.define(["com/rikosjett/localVSCodeDemo/controller/BaseController"], function (Controller) {
     "use strict";
 
-    return Controller.extend("com.rikosjett.localVSCodeDemo.controller.MainView", {});
+    return Controller.extend("com.rikosjett.localVSCodeDemo.controller.MainView", {
+        
+        onInit: function(){
+            var dataModel = this.getOwnerComponent().getModel();
+            this.getView().setModel(dataModel, "DataModel");
+        },
+
+        pokemonListFactory: function(sId, oContext){
+            console.log(sId, oContext)
+            console.log(this.getModel().getData())
+            console.log(oContext.getPath())
+            
+            console.log(this.getModel().getProperty("/pokemon"))
+            console.log(oContext.getProperty("/name"))
+        }
+    });
 });
